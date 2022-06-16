bd9db116d1335c3df03bfc004d2d0ab822a23238
dark-flames <dark_flames@outlook.com>
2022-06-16 13:45:09 +0900

    TEST: add tests

--- src/list.rs
+++ src/list.rs
@@ -196,19 +196,3 @@ impl<
     type Result = Result;
 }
 
-
-#[test]
-fn test_list() {
-    use crate::*;
-    type L1 = List1<Z>;
-    type L2 = Push<S<Z>, L1>;
-    type L3 = Push<S<S<Z>>, L2>;
-
-    type Mapped = Map<AddOn<S<Z>>, L3>;
-
-    assert_ty!(List3<S<Z>, S<S<Z>>, S<S<S<Z>>>>, Mapped);
-    assert_ty!(Contains<S<S<S<Z>>>, Mapped>, True);
-    assert_ty!(Contains<Z, Mapped>, False);
-    assert_ty!(Bind<L3, Pure<List<Nat>>>, L3);
-}
-
---
+++ tests/list.rs
@@ -0,0 +1,16 @@
+use type_level_stlc::*;
+use type_level_stlc::classes::*;
+
+#[test]
+fn test_list() {
+    type L1 = List1<Z>;
+    type L2 = Push<S<Z>, L1>;
+    type L3 = Push<S<S<Z>>, L2>;
+
+    type Mapped = Map<AddOn<S<Z>>, L3>;
+
+    assert_ty!(List3<S<Z>, S<S<Z>>, S<S<S<Z>>>>, Mapped);
+    assert_ty!(Contains<S<S<S<Z>>>, Mapped>, True);
+    assert_ty!(Contains<Z, Mapped>, False);
+    assert_ty!(Bind<L3, Pure<List<Nat>>>, L3);
+}
